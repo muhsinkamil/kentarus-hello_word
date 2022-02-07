a4e44c97531d96518c3064f317cb64e43f60f18a
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-07 14:33:31 +0530

    Add exception handling

--- src/main/java/com/kentarus/hello_word/hello_word/exceptions/CommonException.java
+++ src/main/java/com/kentarus/hello_word/hello_word/exceptions/CommonException.java
@@ -1,5 +1,17 @@
 package com.kentarus.hello_word.hello_word.exceptions;
 
-public class CommonException {
-    
+import com.kentarus.hello_word.hello_word.structures.WordPostResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CommonException  {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest req) {
+        return new ResponseEntity<>(new WordPostResponse("NOK", "Oh oh problem occured !!, Check your body and try again"), HttpStatus.BAD_REQUEST);
+    }
 }
