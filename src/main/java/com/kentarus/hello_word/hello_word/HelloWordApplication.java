860d4204a353e3053769b722c060ef782c25303e
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-07 13:34:55 +0530

    Initialise java project

---
+++ src/main/java/com/kentarus/hello_word/hello_word/HelloWordApplication.java
@@ -0,0 +1,13 @@
+package com.kentarus.hello_word.hello_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kentarus/hello_word/hello_word/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentarus.hello_word.hello_word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
